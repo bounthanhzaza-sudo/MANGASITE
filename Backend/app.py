48a684c00447f89cef9e7e143efd3b90263ba9ca
bounthanhzaza-sudo <bounthanhzaza@gmail.com>
2026-08-31 11:43:45 +0700

    Complete config setup and environment variables

--- Backend/app.py
+++ Backend/app.py
@@ -57,7 +57,7 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST", "localhost"),
             user=os.getenv("DB_USER", "root"),
-            port=int(os.getenv("DB_PORT", 3306)),
+            port=int(os.getenv("DB_PORT", 3307)),
             password=os.getenv("DB_PASSWORD", ""),
             database=os.getenv("DB_NAME", "manga-website"),
             autocommit=True
